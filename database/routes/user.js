7eeaa5d6ec6949c5dc3369896bba3e297db9f97d
dumib <bogdandumitrescu26@yahoo.com>
2022-09-01 18:26:11 +0300

    Initialized database

---
+++ database/routes/user.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const router = express.Router();
+const pool = require('../db');
+
+// ROUTES //
+
+router.post("/register", async (req, res) => {
+    try {
+        const { first_name, last_name,
+                email, birth_date,
+                password } = req.body;
+        
+        const newUser = await pool.query(
+            "INSERT INTO user_info (first_name, last_name, email, birth_date, password) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+            [first_name, last_name, email, birth_date, password]
+        );      
+
+        res.json(newUser.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await pool.query(
+            "SELECT * FROM user_info WHERE email = $1",
+            [email]
+        );
+
+        if (password === user.rows[0].password) {
+            res.json("Login successful");
+        } else {
+            res.json("Login failed");
+        }
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+router.get("/all", async (req, res) => {
+    try {
+        const allUsers = await pool.query("SELECT * FROM user_info");
+        res.json(allUsers.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+router.delete("/delete/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleteUser = await pool.query(
+            "DELETE FROM user_info WHERE id = $1",
+            [id]
+        );
+        res.json("User was deleted!");
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+module.exports = router;
